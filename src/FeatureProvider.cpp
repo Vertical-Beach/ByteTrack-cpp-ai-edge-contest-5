93524acea6c5d0397d941393ba1119049b72ec33
medalotte <medalotte@gmail.com>
2022-02-05 16:43:16 +0000

    update FeatureProvider to calc histgram of hue and saturation

--- src/FeatureProvider.cpp
+++ src/FeatureProvider.cpp
@@ -17,21 +17,30 @@ void byte_track::FeatureProvider::setImage(const cv::Mat &image)
     preproc();
 }
 
-std::vector<float> byte_track::FeatureProvider::getLbpFeature(const byte_track::Rect<float> &rect) const
+const cv::Mat& byte_track::FeatureProvider::getScaledImage() const
+{
+    return image_;
+}
+
+cv::Rect2i byte_track::FeatureProvider::rect2ScaledRect2i(const byte_track::Rect<float> &rect) const
 {
     cv::Rect2i rect2i;
     rect2i.x = std::clamp(static_cast<int>(std::round(rect.x() * cfg_.scale)), 0, image_.size().width - 1);
     rect2i.y = std::clamp(static_cast<int>(std::round(rect.y() * cfg_.scale)), 0, image_.size().height - 1);
     rect2i.width = std::clamp(static_cast<int>(std::round(rect.width() * cfg_.scale)), 0, image_.size().width - rect2i.x);
     rect2i.height = std::clamp(static_cast<int>(std::round(rect.height() * cfg_.scale)), 0, image_.size().height - rect2i.y);
+    return rect2i;
+}
 
-    const cv::Mat rected_lbp_mat = lbp_mat_(rect2i);
-    const float range[] = {0, static_cast<float>(cfg_.n_lbp_feature_hist_bins)};
-    const float *r = range;
+std::vector<float> byte_track::FeatureProvider::getLbpFeature(const byte_track::Rect<float> &rect) const
+{
+    return getLbpFeature(rect2ScaledRect2i(rect));
+}
 
-    cv::Mat hist;
-    cv::calcHist(&rected_lbp_mat, 1, 0, cv::Mat(), hist, 1, &cfg_.n_lbp_feature_hist_bins, &r);
-    cv::normalize(hist, hist, 0.0, 1.0, cv::NORM_MINMAX, -1, cv::Mat());
+std::vector<float> byte_track::FeatureProvider::getLbpFeature(const cv::Rect2i &scaled_rect) const
+{
+    cv::Mat hist = calcHist(lbp_mat_(scaled_rect), cfg_.lbp_feature_hist_range_lower,
+                            cfg_.lbp_feature_hist_range_upper, cfg_.n_lbp_feature_hist_bins);
 
     std::vector<float> hist_v(hist.rows);
     for (int hi = 0; hi < hist.rows; hi++)
@@ -41,8 +50,52 @@ std::vector<float> byte_track::FeatureProvider::getLbpFeature(const byte_track::
     return hist_v;
 }
 
+std::pair<std::vector<float>, std::vector<float>> byte_track::FeatureProvider::getColorFeature(const byte_track::Rect<float> &rect) const
+{
+    return getColorFeature(rect2ScaledRect2i(rect));
+}
+
+std::pair<std::vector<float>, std::vector<float>> byte_track::FeatureProvider::getColorFeature(const cv::Rect2i &scaled_rect) const
+{
+    cv::Mat hue_hist = calcHist(hue_(scaled_rect), 0, 180, cfg_.n_hue_hist_bins);
+    cv::Mat saturation_hist = calcHist(saturation_(scaled_rect), cfg_.saturation_hist_range_lower,
+                                       cfg_.saturation_hist_range_upper, cfg_.n_saturation_hist_bins);
+
+    std::pair<std::vector<float>, std::vector<float>> hist_v;
+    hist_v.first.resize(hue_hist.rows);
+    for (int hi = 0; hi < hue_hist.rows; hi++)
+    {
+        hist_v.first[hi] = hue_hist.ptr<float>(hi)[0];
+    }
+
+    hist_v.second.resize(saturation_hist.rows);
+    for (int hi = 0; hi < saturation_hist.rows; hi++)
+    {
+        hist_v.second[hi] = saturation_hist.ptr<float>(hi)[0];
+    }
+
+    return hist_v;
+}
+
 void byte_track::FeatureProvider::preproc()
 {
-    cv::cvtColor(image_, gray_, CV_RGB2GRAY);
-    LBPFeatureExtractor::exec(gray_, lbp_mat_, cfg_.lbp_type);
+    cv::Mat gray, hsv;
+    std::vector<cv::Mat> rected_hsv_mat_v;
+
+    cv::cvtColor(image_, hsv, CV_RGB2HSV);
+    cv::split(hsv, rected_hsv_mat_v);
+    hue_ = rected_hsv_mat_v[0].clone();
+    saturation_ = rected_hsv_mat_v[1].clone();
+
+    cv::cvtColor(image_, gray, CV_RGB2GRAY);
+    LBPFeatureExtractor::exec(gray, lbp_mat_, cfg_.lbp_type);
+}
+
+cv::Mat byte_track::FeatureProvider::calcHist(const cv::Mat &mat, const int &range_lower, const int &range_upper, const int &bins) const
+{
+    const float range[] = {static_cast<float>(range_lower), static_cast<float>(range_upper)};
+    const float *r = range;
+    cv::Mat hist;
+    cv::calcHist(&mat, 1, 0, cv::Mat(), hist, 1, &bins, &r);
+    return hist;
 }
