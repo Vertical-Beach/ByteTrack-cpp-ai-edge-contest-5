70b1410235207b637884e442c3ec612961e3e8d0
medalotte <medalotte@gmail.com>
2022-02-13 12:59:50 +0000

    add app/generate_submit_file/run.bash

---
+++ app/generate_submit_file/run.bash
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+function exit_with_invalid_args () {
+    echo "Usage 1: $0 <videos directory path> <video name prefix> json <detection results directory path>"
+    echo "Usage 2: $0 <videos directory path> <video name prefix> dpu <modelconfig .prototxt> <modelfile .xmodel>"
+    exit 1
+}
+
+function build () {
+    mkdir -p $1/build && cd $1/build
+    cmake ..
+    make -j
+    cd $1
+}
+
+if [[ $# < 3 ]]; then
+    exit_with_invalid_args
+fi
+
+video_dir=$1
+prefix=$2
+mode=$3
+
+script_dir=`cd $(dirname $0); pwd`
+video_list=`find $video_dir -type f | grep "\.avi"`
+
+if [ $mode == "json" ]; then
+    if [ $# -ne 4 ]; then
+        exit_with_invalid_args
+    fi
+    build $script_dir
+    for video in $video_list; do
+        $script_dir/build/generate_submit_file $video $mode $4
+    done
+elif [ $mode == "dpu" ]; then
+    if [ $# -ne 6 ]; then
+        exit_with_invalid_args
+    fi
+    build $script_dir
+    for video in $video_list; do
+        $script_dir/build/generate_submit_file $video $mode $4 $5
+    done
+else
+    echo "Invalid mode is specified: $mode"
+    exit_with_invalid_args
+fi
+
+python3 $script_dir/combine_submit.py $prefix
+ls -1 | grep -E "^prediction\_$prefix\_[0-9]{2}\.json$" | xargs rm
